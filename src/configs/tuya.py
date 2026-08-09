6d274c309fb2588950039e9ca8b19803e2983d94
Undore <deronuno@outlook.com>
2026-08-09 16:50:50 +0300

    Update tuya.py

--- src/configs/tuya.py
+++ src/configs/tuya.py
@@ -3,4 +3,4 @@ from datetime import timedelta
 
 class TuyaConfig:
     class quotas:
-        api_call_cooldown: timedelta = timedelta(minutes=2)
+        api_call_cooldown: timedelta = timedelta(minutes=1, seconds=30)
